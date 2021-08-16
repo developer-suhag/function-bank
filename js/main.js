ddcd7f9f6ea76b295522809f1b030960251df1df
developer-suhag <developersuhag@gmail.com>
2021-08-16 12:01:20 +0600

    total

--- js/main.js
+++ js/main.js
@@ -8,27 +8,40 @@ function getInputValue(inputId) {
 
 }
 
+function updateTotal(totalField, amount) {
+
+    const previousTotal = document.getElementById(totalField);
+    const previousTotalText = previousTotal.innerText
+    const previousAmount = parseFloat(previousTotalText);
+
+    const totalAmount = previousAmount + amount;
+    previousTotal.innerText = totalAmount;
+}
+
 // diposit 
 
 document.getElementById('deposit-button').addEventListener('click', function () {
+
+
+
     // get the diposit amount 
-    const previousDiposit = document.getElementById('diposit-total');
-    const previousDipositText = previousDiposit.innerText
-    const previousDipositAmount = parseFloat(previousDipositText);
-
-
-    // set the diposit amount 
 
     /*     const currentDiposit = document.getElementById('deposit-input');
         const currentDipositText = currentDiposit.value;
         const currentDipositAmount = parseFloat(currentDipositText) */
     const currentDipositAmount = getInputValue('deposit-input')
 
-    // update diposit amount 
+    // get and update diposit amount 
+    /* 
+        const previousDiposit = document.getElementById('diposit-total');
+        const previousDipositText = previousDiposit.innerText
+        const previousDipositAmount = parseFloat(previousDipositText);
 
-    const dipositTotalAmount = previousDipositAmount + currentDipositAmount;
+        const dipositTotalAmount = previousDipositAmount + currentDipositAmount;
+        previousDiposit.innerText = dipositTotalAmount; */
 
-    previousDiposit.innerText = dipositTotalAmount;
+    updateTotal('diposit-total', currentDipositAmount);
+
 
     // update Total Balance 
 
@@ -46,20 +59,24 @@ document.getElementById('deposit-button').addEventListener('click', function () 
 // withdraw 
 
 document.getElementById('withdraw-button').addEventListener('click', function () {
+    /*
     // get withdraw amount 
+    const currentWithdraw = document.getElementById('withdraw-input')
+    const currentWithdrawText = currentWithdraw.value;
+    const currentWithdrawAmount = parseFloat(currentWithdrawText); 
+    */
+    const currentWithdrawAmount = getInputValue('withdraw-input')
+
+    // get and update withdraw amount 
+    /*  
     const previousWithdraw = document.getElementById('withdraw-total')
     const previousWithdrawText = previousWithdraw.innerText;
     const previousWithdrawAmount = parseFloat(previousWithdrawText)
-
-    /*    // set the withdraw amount 
-       const currentWithdraw = document.getElementById('withdraw-input')
-       const currentWithdrawText = currentWithdraw.value;
-       const currentWithdrawAmount = parseFloat(currentWithdrawText); */
-    const currentWithdrawAmount = getInputValue('withdraw-input')
-
-    // update withdraw amount 
     const totalWithdrawAmount = previousWithdrawAmount + currentWithdrawAmount;
-    previousWithdraw.innerText = totalWithdrawAmount;
+    previousWithdraw.innerText = totalWithdrawAmount; 
+     */
+
+    updateTotal('withdraw-total', currentWithdrawAmount)
 
     // reduce the total balance 
     const previousTotal = document.getElementById('account-balance');
