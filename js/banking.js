970285311a08ffac932381dfeefb8eb605632e47
developer-suhag <developersuhag@gmail.com>
2021-08-16 23:05:22 +0600

    error message

--- js/banking.js
+++ js/banking.js
@@ -24,11 +24,11 @@ function getCurrentBalance() {
     const previousBalanceText = previousBalance.innerText;
     const convertPreviousBalance = parseFloat(previousBalanceText);
     return convertPreviousBalance;
-}
+};
 
 // get and update total balance 
 function updateBalance(balance, isPositve) {
-    debugger;
+    // debugger;
     const previousBalance = document.getElementById('account-balance');
     /* const previousBalanceText = previousBalance.innerText;
     const convertPreviousBalance = parseFloat(previousBalanceText); */
@@ -41,6 +41,30 @@ function updateBalance(balance, isPositve) {
         const totalBalance = convertPreviousBalance - balance;
         previousBalance.innerText = totalBalance;
     }
+};
+
+// error message 
+function errorMessage(errorId) {
+    // debugger;
+    const errorElement = document.getElementById(errorId)
+    errorElement.innerText = 'Error! Please give a Valid Amount'
+
+};
+
+function errorRemove(errorId) {
+    const errorElement = document.getElementById(errorId)
+    errorElement.innerText = ''
+
+};
+
+function balanceError(errorId) {
+    const errorElement = document.getElementById(errorId)
+    errorElement.innerText = "Oops! You can't withdraw Monay more than your total balance"
+}
+
+function balanceErrorRemove(errorId) {
+    const errorElement = document.getElementById(errorId)
+    errorElement.innerText = ""
 }
 
 
@@ -50,6 +74,12 @@ document.getElementById('deposit-button').addEventListener('click', function () 
     if (inputAmount > 0) {
         updateAmount('diposit-total', inputAmount);
         updateBalance(inputAmount, true);
+    };
+
+    if (inputAmount < 0) {
+        errorMessage('deposit-error')
+    } else {
+        errorRemove('deposit-error')
     }
 
 });
@@ -61,5 +91,18 @@ document.getElementById('withdraw-button').addEventListener('click', function ()
     if ((inputAmount > 0) && (inputAmount < currentBalance)) {
         updateAmount('withdraw-total', inputAmount);
         updateBalance(inputAmount, false);
+    }
+    debugger;
+
+    if ((inputAmount > 0) && (inputAmount > currentBalance)) {
+        balanceError('withdraw-balance-error');
+    } else {
+        balanceErrorRemove('withdraw-balance-error')
+    }
+
+    if (inputAmount < 0) {
+        errorMessage('withdraw-error')
+    } else {
+        errorRemove('withdraw-error')
     }
 });
